14710638bdf611310d4b5665e76b683aea4f81a1
michaelslice <mjsens523@gmail.com>
2024-07-10 01:29:54 -0500

    Working on Equity Screener Logic

--- src/equityscreener/views.py
+++ src/equityscreener/views.py
@@ -4,16 +4,34 @@ from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
 import yfinance as yf
+import yahoo_fin.stock_info as si
+import requests
+import os
+from dotenv import load_dotenv
 
-def get_msft(request):
-    msft = yf.Ticker("MSFT")
-    stock_info = msft.info
-    data = {
-        'name': stock_info.get('shortName', 'N/A'),
-        'symbol': stock_info.get('symbol', 'N/A'),
-        'currentPrice': stock_info.get('regularMarketPrice', 'N/A'),
-        'marketCap': stock_info.get('marketCap', 'N/A'),
-    }
-    return JsonResponse(data)
+load_dotenv()
+MY_ENV_VAR = os.getenv("ALPHA_VANTAGE_API_KEY")
 
+@api_view(['GET'])
+def get_screener(request):
+    
+    if request.method == "GET":
+               
+        symbol = request.GET.get("ticker")
+        interval = "5min"
+                
+        if not symbol:
+            return JsonResponse({"Error": "Invalid Symbol"}, status=400)
+                
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={MY_ENV_VAR}'
+        try:
+            r = requests.get(url)
+            r.raise_for_status() # If Bad HTTP Response 
+            data = r.json()
 
+            return JsonResponse(data, safe=False)
+ 
+        except Exception as e:
+            return JsonResponse({"Error": str(e)}, status=400)
+    else:
+        return JsonResponse({"Error": "Invalid Request Method"}, status=400)
