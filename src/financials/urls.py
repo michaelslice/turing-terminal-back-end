fa5d1c09bf50751284113b72b4ba5e1816725609
michaelslice <mjsens523@gmail.com>
2024-07-18 03:07:14 -0500

    Added Logic for Financial Retrieval

--- src/financials/urls.py
+++ src/financials/urls.py
@@ -5,6 +5,8 @@ from . import views
 @note urlpatterns: Contains a list of URL pattern mappings for the Django project
 '''
 urlpatterns = [
-   path('', views.get_msft, name="test"),
-   
+   path('getbalancesheet/', views.get_balance_sheet, name="getbalancesheet"),
+   path('getcashflow/', views.get_cash_flow, name="getcashflow"),
+   path('getincomestatement/', views.get_income_statement, name="getincomestatement"),
+
 ]
--- src/financials/views.py
+++ src/financials/views.py
@@ -4,16 +4,66 @@ from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
 import yfinance as yf
-
-def get_msft(request):
-    msft = yf.Ticker("MSFT")
-    stock_info = msft.info
-    data = {
-        'name': stock_info.get('shortName', 'N/A'),
-        'symbol': stock_info.get('symbol', 'N/A'),
-        'currentPrice': stock_info.get('regularMarketPrice', 'N/A'),
-        'marketCap': stock_info.get('marketCap', 'N/A'),
-    }
-    return JsonResponse(data)
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+load_dotenv()
+MY_ENV_VAR = os.getenv("ALPHA_VANTAGE_API_KEY")
 
 
+@api_view(['GET'])
+def get_balance_sheet(request):
+    
+    if request.method == "GET":
+
+        ticker = request.GET.get("ticker")
+
+        url = f'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol={ticker}&apikey={MY_ENV_VAR}'
+
+        r = requests.get(url)
+        data = r.json()        
+        data = pd.DataFrame(data['annualReports'])
+        
+        return JsonResponse(data.to_dict(orient="records"), safe=False)
+
+    else:
+        return JsonResponse({"Error": "Invalid Request Method"}, status=400)
+
+
+@api_view(['GET'])
+def get_cash_flow(request):
+    
+    if request.method == "GET":
+
+        ticker = request.GET.get("ticker")
+
+        url = f'https://www.alphavantage.co/query?function=CASH_FLOW&symbol={ticker}&apikey={MY_ENV_VAR}'
+
+        r = requests.get(url)
+        data = r.json()        
+        data = pd.DataFrame(data['annualReports'])
+        
+        return JsonResponse(data.to_dict(orient="records"), safe=False)
+
+    else:
+        return JsonResponse({"Error": "Invalid Request Method"}, status=400)
+
+
+@api_view(['GET'])
+def get_income_statement(request):
+    
+    if request.method == "GET":
+
+        ticker = request.GET.get("ticker")
+
+        url = f'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={ticker}&apikey={MY_ENV_VAR}'
+
+        r = requests.get(url)
+        data = r.json()        
+        data = pd.DataFrame(data['annualReports'])
+        
+        return JsonResponse(data.to_dict(orient="records"), safe=False)
+
+    else:
+        return JsonResponse({"Error": "Invalid Request Method"}, status=400)
