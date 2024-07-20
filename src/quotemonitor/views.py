e6a81f30551783b1a9f3319f018385c3534b60e5
michaelslice <mjsens523@gmail.com>
2024-07-20 03:42:16 -0500

    Working on Quote Monitor End Point

--- src/quotemonitor/views.py
+++ src/quotemonitor/views.py
@@ -5,18 +5,49 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 import yfinance as yf
 from quotemonitor.model import Ticker
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+load_dotenv()
+MY_ENV_VAR = os.getenv("ALPHA_VANTAGE_API_KEY")
+
 
 def upload_ticker(request):
     
-    ticker = request.GET.get("ticker")
-    print(ticker)
-    
-    if request.method == "POST":
+    if request.method == "GET":
+        
+        ticker = request.GET.get("ticker")
+        
+        url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo'
+        # url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={MY_ENV_VAR}'
+        r = requests.get(url)
+        data = r.json()
+        data = pd.DataFrame(data)
         
         #new_ticker = Ticker(symbol="AAPL", last="12", bid="12", ask="12", change_percent="12", volume="12")
-        #new_ticker.save()
+        #new_ticker.save()     
         
-        return JsonResponse({"TEST": "test"}, status=200)
+        quote_data = data['Global Quote']
+        df = pd.DataFrame([quote_data])  
+
+        # Change names
+        columns = {
+        '01. symbol': 'symbol',
+        '02. open': 'open',
+        '03. high': 'high',
+        '04. low': 'low',
+        '05. price': 'price',
+        '06. volume': 'volume',
+        '07. latest trading day': 'latestTradingDay',
+        '08. previous close': 'previousClose',
+        '09. change': 'change',
+        '10. change percent': 'changePercent'
+        }
+
+        df = df.rename(columns=columns)
+
+        return JsonResponse(df.to_dict(orient="records"), status=200, safe=False) 
 
     else:
         return JsonResponse({"Error": "Invalid Request Method"}, status=400)
