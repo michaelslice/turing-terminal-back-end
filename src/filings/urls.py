635bbbd5c33edf7bb9caf56f20b0795e29733e71
michaelslice <mjsens523@gmail.com>
2024-07-22 13:33:49 -0500

    Finished Filings

--- src/filings/urls.py
+++ src/filings/urls.py
@@ -5,6 +5,6 @@ from . import views
 @note urlpatterns: Contains a list of URL pattern mappings for the Django project
 '''
 urlpatterns = [
-   path('', views.get_msft, name="test"),
+   path('getfilings/', views.get_filings, name="getfilings"),
    
 ]
--- src/filings/views.py
+++ src/filings/views.py
@@ -4,16 +4,50 @@ from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
 import yfinance as yf
-
-def get_msft(request):
-    msft = yf.Ticker("MSFT")
-    stock_info = msft.info
-    data = {
-        'name': stock_info.get('shortName', 'N/A'),
-        'symbol': stock_info.get('symbol', 'N/A'),
-        'currentPrice': stock_info.get('regularMarketPrice', 'N/A'),
-        'marketCap': stock_info.get('marketCap', 'N/A'),
-    }
-    return JsonResponse(data)
+from sec_api import QueryApi
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import yahoo_fin.stock_info as si
+load_dotenv()
+MY_ENV_VAR = os.getenv("EDGAR_SEC_API_KEY")
 
 
+@api_view(['GET'])
+def get_filings(request):
+    if request.method == "GET":
+        ticker = request.GET.get("ticker")
+        if not ticker:
+            return Response({"Error": 'No Stock Ticker Provided'}, status=400)
+
+        queryApi = QueryApi(api_key=MY_ENV_VAR)
+        query = {
+            "query": f"ticker:{ticker}",
+            "from": "0",
+            "size": "10",
+            "sort": [{"filedAt": {"order": "desc"}}]
+        }
+
+        response = queryApi.get_filings(query)
+        if 'filings' not in response:
+            return Response({"Error": "No filings found for the provided ticker."}, status=404)
+
+        metadata = pd.DataFrame.from_records(response['filings'])
+
+        # Keep only necessary columns
+        metadata = metadata[[
+            "ticker",
+            "formType",
+            "linkToFilingDetails",
+            "description",
+            "filedAt",
+            "periodOfReport"
+        ]]
+        # Replace null values with empty string
+        metadata = metadata.fillna('')
+        
+        return Response(metadata.to_dict(orient="records"), status=200)
+    
+    else:
+        return Response({"Error": 'Invalid request method.'}, status=405)
