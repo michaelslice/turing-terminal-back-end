9ddad033d7d22a93b2066515a1095285728ad14e
michaelslice <mjsens523@gmail.com>
2024-07-23 06:39:48 -0500

    Working on Equity Screener

--- src/companyevents/views.py
+++ src/companyevents/views.py
@@ -4,9 +4,51 @@ from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
 import yfinance as yf
+from sec_api import QueryApi
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import yahoo_fin.stock_info as si
+load_dotenv()
+MY_ENV_VAR = os.getenv("EDGAR_SEC_API_KEY")
 
+'''
+
+
+
+https://sec-api.io/docs/form-8k-data-search-api#request-parameters
+
+'''
 def get_company_events(request):
 
+    if request.method == "GET":
+        
+        ticker = request.GET.get("ticker")
 
+        queryApi = QueryApi(api_key=MY_ENV_VAR)
+        query = {
+        "query": f'ticker:{ticker}',
+        "from": "0",
+        "size": "50"
+        }
 
-    return JsonResponse("data")
+        response = queryApi.get_filings(query)
+
+        metadata = pd.DataFrame.from_records(response['filings'])
+
+        metadata = metadata[[
+            'ticker',
+            'formType',
+            'linkToTxt',
+            'description',
+            'filedAt',
+            'periodOfReport'
+        ]]
+        # Replace null values with empty string
+        metadata = metadata.fillna('')
+    
+        return JsonResponse(metadata.to_dict(orient="records"), safe=False, status=200)
+
+    else:
+        return Response({"Error": 'Invalid request method.'}, status=405)
--- src/equityscreener/views.py
+++ src/equityscreener/views.py
@@ -3,35 +3,63 @@ from rest_framework.decorators import api_view
 from rest_framework import generics, status
 from django.http import HttpResponse
 from django.http import JsonResponse
-import yfinance as yf
 import yahoo_fin.stock_info as si
-import requests
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-MY_ENV_VAR = os.getenv("ALPHA_VANTAGE_API_KEY")
+'''
+
+The function num_to_numeric(market_cap_str) is used to convert
+a string to a numeric value
+
+@param market_cap_str: Represents a market cap (3.14T)
+
+@return: This function will return the numeric conversion of the string
+
+@notes: Supports only millions, billions, and trillions into this conversion
+Million: 10^6(1,000,000)
+Billion: 10^9(1,000,000,000)
+Trillion: 10^12(1,000,000,000,000)
+'''
+def num_to_numeric(market_cap_str):
+    units = {'B': 10**9, 'M': 10**6, 'T': 10**12}
+    if market_cap_str[-1] in units: # Check if last character is one of the keys in units
+        return float(market_cap_str[:-1]) * units[market_cap_str[-1]]
+    return float(market_cap_str)
+            
 
 @api_view(['GET'])
 def get_screener(request):
     
     if request.method == "GET":
                
-        symbol = request.GET.get("ticker")
-        interval = "5min"
+        ticker = request.GET.get("ticker")
+        operand = request.GET.get("operand")
+        value = request.GET.get("value")
+        tickers = si.tickers_sp500()     
+        
+        print(ticker, operand, value)
+        
+        if ticker != None and operand == None and value == None:
+   
+            for ticker in tickers:
+                ticker_price = round(si.get_live_price(ticker), 2)
+                my_dic = {ticker: ticker_price}
+        
+        elif ticker and operand and value != None:
+            
+            for ticker in tickers:
+                ticker_price = round(si.get_live_price(ticker), 2)
+                ticker_market_cap = si.get_stats_valuation(ticker)
+                metadeta = ticker_market_cap[['Current']]
+                metadeta["Numeric"] = metadeta['Current'].apply(num_to_numeric)
                 
-        if not symbol:
-            return JsonResponse({"Error": "Invalid Symbol"}, status=400)
+                if metadeta['Numeric'][0] > value:
                 
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={MY_ENV_VAR}'
-        try:
-            r = requests.get(url)
-            r.raise_for_status() # If Bad HTTP Response 
-            data = r.json()
-
-            return JsonResponse(data, safe=False)
- 
-        except Exception as e:
-            return JsonResponse({"Error": str(e)}, status=400)
+                    my_dic = {ticker: ticker_price}
+        
+        
+        return JsonResponse(my_dic(orient="records"), safe=False, status=200)
+    
+        # return JsonResponse(my_dic(orient="records"), safe=False, status=200)
+    
     else:
         return JsonResponse({"Error": "Invalid Request Method"}, status=400)
